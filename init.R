15050baeb79ad0e7d260c750cfb1e0440b099d88
Nicole Neo <84568532+nicolenwd@users.noreply.github.com>
2021-05-28 22:51:45 +0800

    Upload files

---
+++ init.R
@@ -0,0 +1,16 @@
+# R script to install packages if not already installed
+
+my_packages = c("tidyverse", "dplyr", "readr", "data.table",
+                "httr",
+                "sp", "sf", "rgdal",
+                "leaflet", "leaflet.extras",
+                "htmltools", "htmlwidgets", "rvest",
+                "shiny", "shinythemes", "DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
